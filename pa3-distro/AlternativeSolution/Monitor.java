0e04be8b391b515834ef77e6389bd99475a6382a
dabris <dabris16.x@gmail.com>
2019-03-31 23:45:20 -0400

    task 2 in progress

---
+++ pa3-distro/AlternativeSolution/Monitor.java
@@ -0,0 +1,101 @@
+package AlternativeSolution;
+
+/**
+ * Class Monitor
+ * To synchronize dining philosophers.
+ *
+ * @author Serguei A. Mokhov, mokhov@cs.concordia.ca
+ */
+public class Monitor
+{
+	/*
+	 * ------------
+	 * Data members
+	 * ------------
+	 */
+	private int chopstickNum;
+	private boolean eating[];//indicates if a given philosopher is eating or not
+	private boolean silent=true;//indicates if anyone is talking
+	private boolean sleeping=false;
+	/**
+	 * Constructor
+	 */
+	public Monitor(int piNumberOfPhilosophers)
+	{
+		// TODO: set appropriate number of chopsticks based on the # of philosophers
+		chopstickNum=piNumberOfPhilosophers;
+		eating=new boolean[piNumberOfPhilosophers];//each philosopher has a boolean that indicates whether they are eating
+		
+	}
+
+	/*
+	 * -------------------------------
+	 * User-defined monitor procedures
+	 * -------------------------------
+	 */
+	public synchronized void signalSleep() {
+		sleeping=true;
+	}
+	/**
+	 * Grants request (returns) to eat when both chopsticks/forks are available.
+	 * Else forces the philosopher to wait()
+	 */
+	public synchronized void pickUp(final int piTID)
+	{	
+		while(true)//while the philosopher haven't eaten
+			//if neighbors are not eating, take the chopsticks and eat
+		if(eating[(piTID+chopstickNum)%chopstickNum]!=true && eating[(piTID+chopstickNum-2)%chopstickNum]!= true) {
+			//change status to eating
+			eating[piTID-1]=true;
+			//break out of the loop when the philosopher is eating
+			break;
+		}else {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * When a given philosopher's done eating, they put the chopstiks/forks down
+	 * and let others know they are available.
+	 */
+	public synchronized void putDown(final int piTID)
+	{	
+		eating[piTID-1]=false;//set the eating status of the philosopher to false
+		notifyAll();//let others check if they can eat
+	}
+
+	/**
+	 * Only one philosopher at a time is allowed to philosophy
+	 * (while she is not eating).
+	 */
+	public synchronized void requestTalk()
+	{
+		
+		while(!silent) {//while someone is talking
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		silent=false;//else indicate that silent is false and talk
+	}
+
+	/**
+	 * When one philosopher is done talking stuff, others
+	 * can feel free to start talking.
+	 */
+	public synchronized void endTalk()
+	{
+		silent=true;//it's silent again and someone can talk
+		notifyAll();//signal that someone can talk
+	}
+}
+
+// EOF
---
+++ pa3-distro/AlternativeSolution/Philosopher.java
@@ -0,0 +1,140 @@
+package AlternativeSolution;
+import common.BaseThread;
+import java.util.concurrent.ThreadLocalRandom;
+/**
+ * Class Philosopher.
+ * Outlines main subrutines of our virtual philosopher.
+ *
+ * @author Serguei A. Mokhov, mokhov@cs.concordia.ca
+ */
+public class Philosopher extends BaseThread
+{
+	/**
+	 * Max time an action can take (in milliseconds)
+	 */
+	public static final long TIME_TO_WASTE = 1000;
+
+	/**
+	 * The act of eating.
+	 * - Print the fact that a given phil (their TID) has started eating.
+	 * - yield
+	 * - Then sleep() for a random interval.
+	 * - yield
+	 * - The print that they are done eating.
+	 */
+	public void eat()
+	{
+		try
+		{
+			System.out.println("Phil "+getTID()+" has started eating");//phil started to eat
+			yield();
+			sleep((long)(Math.random() * TIME_TO_WASTE));
+			yield();
+			System.out.println("Phil "+getTID()+" is done eating");//phil is done eating
+		}
+		catch(InterruptedException e)
+		{
+			System.err.println("Philosopher.eat():");
+			DiningPhilosophers.reportException(e);
+			System.exit(1);
+		}
+	}
+
+	/**
+	 * The act of thinking.
+	 * - Print the fact that a given phil (their TID) has started thinking.
+	 * - yield
+	 * - Then sleep() for a random interval.
+	 * - yield
+	 * - The print that they are done thinking.
+	 */
+	public void think()
+	{
+		try
+		{
+			System.out.println("Phil "+getTID()+" has started thinking");//phil started to think
+			yield();
+			sleep((long)(Math.random() * TIME_TO_WASTE));
+			yield();
+			System.out.println("Phil "+getTID()+" is done thinking");//phil is done eating
+		}
+		catch(InterruptedException e)
+		{
+			System.err.println("Philosopher.eat():");
+			DiningPhilosophers.reportException(e);
+			System.exit(1);
+		}
+	}
+
+	/**
+	 * The act of talking.
+	 * - Print the fact that a given phil (their TID) has started talking.
+	 * - yield
+	 * - Say something brilliant at random
+	 * - yield
+	 * - The print that they are done talking.
+	 */
+	public void talk()
+	{
+		System.out.println("Phil "+getTID()+" has started talking");//phil started to talk
+		yield();
+		saySomething();//say something brilliant at random
+		yield();
+		System.out.println("Phil "+getTID()+" is done talking");//phil is done talking
+	}
+
+	/**
+	 * No, this is not the act of running, just the overridden Thread.run()
+	 */
+	public void run()
+	{
+		for(int i = 0; i < DiningPhilosophers.DINING_STEPS; i++)
+		{
+			DiningPhilosophers.soMonitor.pickUp(getTID());
+
+			eat();
+
+			DiningPhilosophers.soMonitor.putDown(getTID());
+
+			think();
+
+			/*
+			 * TODO:
+			 * A decision is made at random whether this particular
+			 * philosopher is about to say something terribly useful.
+			 */
+			if(ThreadLocalRandom.current().nextInt(0, 5)==0)//25% chance for a philosopher to say something terribly useful
+			{
+				DiningPhilosophers.soMonitor.requestTalk();//request to talk
+				talk();
+				DiningPhilosophers.soMonitor.endTalk();//end talking
+			}
+
+			yield();
+		}
+	} // run()
+
+	/**
+	 * Prints out a phrase from the array of phrases at random.
+	 * Feel free to add your own phrases.
+	 */
+	public void saySomething()
+	{
+		String[] astrPhrases =
+		{
+			"Eh, it's not easy to be a philosopher: eat, think, talk, eat...",
+			"You know, true is false and false is true if you think of it",
+			"2 + 2 = 5 for extremely large values of 2...",
+			"If thee cannot speak, thee must be silent",
+			"My number is " + getTID() + ""
+		};
+
+		System.out.println
+		(
+			"Philosopher " + getTID() + " says: " +
+			astrPhrases[(int)(Math.random() * astrPhrases.length)]
+		);
+	}
+}
+
+// EOF
